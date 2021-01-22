238586f2edd8b3d29798bc61e6cbf6dce683a221
Ribson Roy <ribson@riafy.me>
2021-01-22 18:07:04 +0530

    setting splash screen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {AuthContext} from '../context/AuthContext';
 
 const SplashScreen = () => {
+  const {checkAuthState} = useContext(AuthContext);
+
+  useEffect(() => {
+    checkAuthState();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <View>
       <Text>SplashScreen</Text>

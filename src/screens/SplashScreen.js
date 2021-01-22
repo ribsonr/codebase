3e8c5b28e50fb0927bc3ba40ebf5d3f0928ac503
ANANDHU A <anandhu.ag@riafy.me>
2021-01-22 18:09:19 +0530

    minor fixes

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {AuthContext} from '../context/AuthContext';
 
 const SplashScreen = () => {
+  const {checkAuthState} = useContext(AuthContext);
+
+  useEffect(() => {
+    checkAuthState();
+  }, []);
+
   return (
     <View>
       <Text>SplashScreen</Text>
